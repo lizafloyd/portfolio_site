c8cebdd15b3d3f6b11dd5625a38ce37efedce485
Liza Floyd <lizafloyd@gmail.com>
2019-02-06 10:37:05 -0500

    adds technologies section

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -18,20 +18,24 @@ const WorkExperience = () => (
         detail, and learning unbelievably fast. And that's how I like it.
       </p>
       <p>
-        The last few years I've worked for Arc Publishing, a subsidiary of the
-        Washington Post, which is leasing the Post's in-house content management
-        platform out to other publishers around the world. The work is an
-        interesting mix of product, agency, and consultancy work--we control our
-        own roadmaps but do answer to clients--so I've worn every hat you can
-        imagine. I've owned projects, joined sales calls, lead teams, faced down
-        timelines, managed client accounts, developed complex renderings--you
-        name it, I've done it. At Arc, I started as a junior developer, and have
-        since held titles as Solutions Engineer, midlevel Developer, and now as
-        technical lead, delivering a full site build out to a client in Europe.
+        The last few years I've worked for Arc Publishing, the SAAS arm of the
+        Washington Post. The work is an interesting mix of product, agency, and
+        consultancy work, so I've worn every hat you can imagine. I've owned
+        projects, joined sales calls, lead teams, faced down timelines, managed
+        client accounts, developed complex renderings--you name it, I've done
+        it. At Arc, I started as a junior developer, and have since held titles
+        as Solutions Engineer, midlevel Developer, and now as technical lead,
+        delivering a full site build out to a client in Europe.
       </p>
       <p>
-        Before that, I've worked in fundraising and as a bartender. And I ought
-        to tell you, not to brag, just so you know: I serve a hell of a Bud.
+        I've worked primarily with React, HTML, CSS, JavaScript, Webpack,
+        JSP/JSTL, SCSS, Enzyme, Jest, and Regex. If you're interested in some of
+        my work, select a partner logo to the left to see their site.
+      </p>
+      <p>
+        Before I was a developer, I've worked in fundraising and as a bartender.
+        And I ought to tell you, not to brag, just so you know: I serve a hell
+        of a Bud.
       </p>
     </div>
   </div>
