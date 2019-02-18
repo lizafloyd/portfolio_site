97a969f4e26ddc88235d0d11e0f811e8aaf661ee
Liza Floyd <lizafloyd@gmail.com>
2019-02-18 17:05:20 -0500

    formats

--- src/components/FunFacts.js
+++ src/components/FunFacts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Map from './Map';
-import { mapPins } from '../constants'
+import {mapPins} from '../constants';
 /**
  * Renders the ContactForm component.
  * @returns {string} - Returns JSX representation of the FunFacts component.
@@ -10,15 +10,22 @@ const FunFacts = () => (
     <div className="small-7">
       <h2 className="subtitle">Fun Facts</h2>
       <ul>
-        <li>I've been all these places <span role="img" aria-label="Point right emoji">👉</span></li>
+        <li>
+          I've been all these places{' '}
+          <span role="img" aria-label="Point right emoji">
+            👉
+          </span>
+        </li>
         <li>Second fun fact</li>
         <li>Another fun fact</li>
-        </ul>
+      </ul>
     </div>
     <div className="small-5">
       <Map
         isMarkerShown
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAP_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${
+          process.env.REACT_APP_MAP_KEY
+        }&v=3.exp&libraries=geometry,drawing,places`}
         loadingElement={<div style={{height: `100%`}} />}
         containerElement={<div style={{height: `300px`}} />}
         mapElement={<div style={{height: `100%`}} />}
--- src/components/Map.js
+++ src/components/Map.js
@@ -7,10 +7,13 @@ import {
 } from 'react-google-maps';
 
 const Map = (props) => (
-    <GoogleMap defaultZoom={1} defaultCenter={{lat: 39.3999, lng: 12.2245}}>
-      {props.isMarkerShown && props.mapPins.map((pin, index) => <Marker key={index} position={{lat: pin.lat, lng: pin.lng}} />)}
-    </GoogleMap>
-  )
+  <GoogleMap defaultZoom={1} defaultCenter={{lat: 39.3999, lng: 12.2245}}>
+    {props.isMarkerShown &&
+      props.mapPins.map((pin, index) => (
+        <Marker key={index} position={{lat: pin.lat, lng: pin.lng}} />
+      ))}
+  </GoogleMap>
+);
 
-export {Map as PureMap}
-export default withScriptjs(withGoogleMap(Map))
+export {Map as PureMap};
+export default withScriptjs(withGoogleMap(Map));
