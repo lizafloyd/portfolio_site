4c6953e054265439b38d8b34c38f7e66064ea042
Liza Floyd <lizafloyd@gmail.com>
2019-03-09 16:20:43 -0500

    Updates

---
+++ precache-manifest.f7ee56b2beb0005c8e05d942dabde597.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "39b1666681d45a3573c578ce510cb3af",
+    "url": "/static/media/washington_post.39b16666.png"
+  },
+  {
+    "revision": "f4dd441ca43ec350a0a1",
+    "url": "/static/css/main.31f9b3bb.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "bbc043e226ecd88534e5",
+    "url": "/static/js/2.bbc043e2.chunk.js"
+  },
+  {
+    "revision": "8d42fb038d8a6f665aa32be60f895d5d",
+    "url": "/static/media/leparisien.8d42fb03.png"
+  },
+  {
+    "revision": "346540d7f4edd07a14a63fdc89424256",
+    "url": "/static/media/raycom.346540d7.png"
+  },
+  {
+    "revision": "137caac2507ce823f46de81b5c3a2e4d",
+    "url": "/static/media/santafe.137caac2.png"
+  },
+  {
+    "revision": "4a1ea470396f424c8da7a0af1c47e078",
+    "url": "/static/media/tgam.4a1ea470.png"
+  },
+  {
+    "revision": "2ce004c1000e873332a717646db702e2",
+    "url": "/static/media/the_lily.2ce004c1.png"
+  },
+  {
+    "revision": "f4dd441ca43ec350a0a1",
+    "url": "/static/js/main.f4dd441c.chunk.js"
+  },
+  {
+    "revision": "68fc815f5cbc690c813f65100555720b",
+    "url": "/static/media/ga.68fc815f.jpg"
+  },
+  {
+    "revision": "c7fa3f4599efbb06c81b9862d9ccd519",
+    "url": "/static/media/projects.c7fa3f45.jpg"
+  },
+  {
+    "revision": "0611aa2ee379945e4d4905dca461d2ae",
+    "url": "/static/media/card_02.0611aa2e.jpeg"
+  },
+  {
+    "revision": "22894424c45af10b31360551e3697c05",
+    "url": "/static/media/profile.22894424.jpg"
+  },
+  {
+    "revision": "1dbf8e2cdb8b568a0128270cac3d8b22",
+    "url": "/static/media/traindoor.1dbf8e2c.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "bbc043e226ecd88534e5",
+    "url": "/static/css/2.63d825db.chunk.css"
+  },
+  {
+    "revision": "cf52471109eb59696f3b33d48c39dc50",
+    "url": "/index.html"
+  }
+];
